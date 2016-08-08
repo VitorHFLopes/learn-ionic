05dbb1e05dc8b816021261dd88718f1959c441d8
Vitor Lopes <vitor@mixd.com.br>
2016-08-08 15:43:01 -0300

    Commit inicial de aprendizagem de Ionic. PS: Ja possui os arquivos para integracao com push-notification

---
+++ www/js/app.js
@@ -0,0 +1,14 @@
+angular.module('starter', ['ionic'])
+ 
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    var push = new Ionic.Push({
+      "debug": true
+    });
+ 
+    push.register(function(token) {
+      console.log("My Device token:",token.token);
+      push.saveToken(token);  // persist the token in the Ionic Platform
+    });
+  });
+})
