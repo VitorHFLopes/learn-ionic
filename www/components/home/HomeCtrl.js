9279d1e486c9dd9e1e9cc388096ac41b7dbbd206
Vitor Hugo Fernandes Lopes <vitor.automec@hotmail.com>
2016-09-21 23:06:47 -0300

    Voltando arquivos de rota e controllers

--- www/components/home/HomeCtrl.js
+++ www/components/home/HomeCtrl.js
@@ -1,17 +1,7 @@
 angular.module('appModule')
 
-  .controller('HomeCtrl', function ($scope, $ionicPush) {
+  .controller('HomeCtrl', function ($scope) {
 
-    $ionicPush.register().then(function(t) {
-      return $ionicPush.saveToken(t);
-    }).then(function(t) {
-      console.log('Token saved:', t.token);
-    });
-
-    $scope.$on('cloud:push:notification', function(event, data) {
-      var msg = data.message;
-      alert(msg.title + ': ' + msg.text);
-    });
   })
 
 ;
