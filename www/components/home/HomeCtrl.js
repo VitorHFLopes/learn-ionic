b506a8014a1b888db8fd02aa92dc910fd94f823c
Vitor Hugo Fernandes Lopes <vitor.automec@hotmail.com>
2016-10-12 19:23:20 -0300

    Adicionando verificação de bluetooth e localização, pois o beacon necessita desse dois recursos para funcionar

--- www/components/home/HomeCtrl.js
+++ www/components/home/HomeCtrl.js
@@ -1,30 +1,64 @@
 angular.module('appModule')
 
-    .controller('HomeCtrl', function ($ionicPlatform, $scope) {
+    .controller('HomeCtrl', function ($ionicPlatform, $ionicPopup, $scope) {
+
 
         $scope.beaconArray = [];
         $scope.showBeacon = false;
 
         $ionicPlatform.ready(function () {
+
+            cordova.plugins.locationManager.isBluetoothEnabled()
+                .then(function(isBluetoothEnabled){ //Retorna um boolean
+                    if (!isBluetoothEnabled) {
+                        $ionicPopup.confirm({
+                            title: 'Por favor, ligue o bluetooth',
+                            template: 'Clique em OK para ligar o bluetooth'
+                        }).then(function (answer) {
+                            if(answer) {
+                                cordova.plugins.locationManager.enableBluetooth();
+                            }
+                        })
+                    }
+                })
+                .fail(function(e) { console.error(e); })
+                .done();
+
+            cordova.plugins.diagnostic.isLocationEnabled(function (isLocationEnabled) { //Retorna um boolean
+                if(!isLocationEnabled) {
+                    $ionicPopup.confirm({
+                        title: 'Por favor, ligue a sua localização',
+                        template: 'Clique em OK para acessar as configurações de localização'
+                    }).then(function (answer) {
+                        if(answer) {
+                            cordova.plugins.diagnostic.switchToLocationSettings();
+                        }
+                    })
+                }
+            }, function (errorMessage) { //Retorna uma string com o erro
+                console.error(errorMessage);
+            });
+
             var delegate = new cordova.plugins.locationManager.Delegate();
 
-            cordova.plugins.locationManager.setDelegate(delegate);
-
-            //verifica se startou a regiao de monitoramento
+            //Verifica se startou a regiao de monitoramento
             delegate.didStartMonitoringForRegion = function(pluginResult) {
                 console.log('startou regiao', pluginResult);
             };
 
-            //determina o estado atual do beacon, dentro ou fora da regiao de monitoramento
+            //Determina o estado atual do beacon, dentro ou fora da regiao de monitoramento
             delegate.didDetermineStateForRegion = function (pluginResult) {
                 console.log('determinou estado', pluginResult);
                 $scope.beaconArray.push(pluginResult);
             };
 
-            //verifica se tem beacons por perto pela proximidade
+            //Verifica se tem beacons por perto pela proximidade
             /*delegate.didRangeBeaconsInRegion = function (pluginResult) {
-             console.log('beacons no range', pluginResult);
-             }*/
+                console.log('beacons no range', pluginResult);
+            };*/
+
+            cordova.plugins.locationManager.setDelegate(delegate);
+
         });
 
         $scope.toShowBeacons = function () {
